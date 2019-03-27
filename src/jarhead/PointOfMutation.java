260cba257569d9aea57249e35091e26ed56e551a
ElectricIsotope <ward.joshua92@yahoo.com>
2019-03-27 01:57:46 +0000

    PointOfMutation: fixed redundant objects

--- src/jarhead/PointOfMutation.java
+++ src/jarhead/PointOfMutation.java
@@ -2,20 +2,19 @@ package jarhead;
 import java.util.*;
 
 public class PointOfMutation{
-	public HashMap<Integer, ConnectionGene> innovationGenes;
+	public Map<Integer, ConnectionGene> innovationGenes;
 	public Genome mascot;
 	public Float highScore;
 	public List<Genome> members;
 	
 	//Constructor
 	public PointOfMutation(Float score, Genome newGenome){	
-		mascot = new Genome(newGenome);//copy as genomes may be removed. this is historic so must be non-volatile
-		innovationGenes = new HashMap<Integer, ConnectionGene>(); //copy " "
-		innovationGenes.putAll(newGenome.getConnectionGenes());
+		mascot = newGenome;//no need to reinstantiate
+		innovationGenes = newGenome.getConnectionGenes();
 
 		highScore = score;//not highScore as not resetting scores on branch.
 		members = new ArrayList<Genome>();
-		members.add(newGenome);
+		members.add(newGenome); //TODO: getter and setters?
 	}
 }
 
