988c3d9073e7a0f864cb3ba8a5356a888603af86
ElectricIsotope <ward.joshua92@yahoo.com>
2019-04-13 00:13:36 -0700

    Ancestors: minor fixes/inline and survival of the fittest niche optimization

--- src/jarhead/Ancestors.java
+++ src/jarhead/Ancestors.java
@@ -23,7 +23,14 @@ public class Ancestors{
 
 	public void migrate(List<Genome> genepool){
 	//migration and placement of genomes.
-		swapOut();//clear members
+		//TODO: increase resources per migration.
+		//dont need to condsider swapin due to OrderOfOperations
+		//	not consistent with actual innovation. some large innovation
+		//	may appear after a long gap in fitness reward. remember fitness
+		//	landscape is not necessarily smooth nor frequent
+		for(PointOfMutation swap : POMs.keySet()){
+			swap.members.clear(); //clear members
+		}
 		for(Genome assignGenome : genepool){
 			PointOfMutation match = POMs.entrySet().parallelStream()
 					.sorted((e1,e2)->e2.getValue().compareTo(e1.getValue()))
@@ -37,7 +44,9 @@ public class Ancestors{
 							    .sorted((p1,p2)->p1.highScore.compareTo(p2.highScore))
 							    .collect(Collectors.toList())){
 			System.out.println("\t " + printing + " With " + printing.members.size() + " @ " + POMs.get(printing) + " Genomes"
-						+ " with score " + printing.highScore + " and mascot: " + printing.mascot);
+						+ " with score " + printing.highScore + " and mascot: " + printing.mascot 
+						+ " with genes: " + printing.mascot.getConnectionGenes().size()
+						+ " and Topology: " + printing.mascot.getNodeGenes().size());
 		}
 		System.out.println("MIGRATION COMPLETE...");
 
@@ -47,7 +56,7 @@ public class Ancestors{
 
 		for(PointOfMutation checkPOM : POMs.keySet()){
 			Optional<Genome> match = checkPOM.members.parallelStream().filter(g->scoreMap.get(g) > checkPOM.highScore)
-								  .filter(g->checkPOM.innovationGenes.keySet().containsAll(g.getConnectionGenes().keySet()))
+								  .filter(g->checkPOM.innovationGenes.keySet().equals(g.getConnectionGenes().keySet()))
 								  .max((g1,g2)->scoreMap.get(g1).compareTo(scoreMap.get(g2)));
 			if(match.isPresent()){
 				System.out.println("OPTIMIZATION..");
@@ -55,16 +64,11 @@ public class Ancestors{
 				POMs.put(replacement, POMs.get(checkPOM));
 				//remove all POMs that have lower high score than
 				//replacement and lineage greater than replacement
-				for(PointOfMutation extinct : POMs.keySet()){
-					if(POMs.get(extinct) >= POMs.get(replacement) && extinct.highScore < replacement.highScore){
-						POMs.remove(extinct);
-					}
-				}
-				POMs.remove(checkPOM);
+				POMs.remove(checkPOM); //remove old unoptimized POM
 
 				//defragment lineage count
-				PointOfMutation buffer = POMs.keySet().stream().findFirst().get();
-				for(PointOfMutation rearrange : POMs.keySet().stream().skip(1).collect(Collectors.toSet())){
+				PointOfMutation buffer = POMs.keySet().parallelStream().findFirst().get();
+				for(PointOfMutation rearrange : POMs.keySet().parallelStream().skip(1).collect(Collectors.toSet())){
 					if(POMs.get(buffer) < POMs.get(rearrange) - 1){
 						POMs.remove(rearrange);
 						POMs.put(rearrange, POMs.get(buffer) + 1);
@@ -93,18 +97,20 @@ public class Ancestors{
 			        branched = true;
 			}
 		}
+		
 		//consider new POMs for migration
 		if(branched){
 			migrate(scoreMap.keySet().parallelStream().collect(Collectors.toList()));
 		}
+		//survival of the fittest (remove niches that overconsume and undercompete)
+			for(PointOfMutation extinct : POMs.keySet()){
+				POMs.entrySet().parallelStream().filter(p->extinct.highScore >= p.getKey().highScore 
+						&& extinct.innovationGenes.size() <= p.getKey().innovationGenes.size() 
+						&& !extinct.equals(p.getKey())
+						&& p.getKey().members.isEmpty())
+				.forEachOrdered(p->POMs.remove(p.getKey()));					
+		}
 	}
-	//nicheOptimization (backtracking weight optimization):
-	//called within (before) speciate
-	//if highscore but no innovation genes remove POM and construct same POM topology 
-	//with superior weight schema and remove all POMs with lineage count > POMs.get(checkPOM)
-	//and score < highscore
-	//
-	//else{
 	/**
 	 * used to declare a niche
 	 */
@@ -131,40 +137,30 @@ public class Ancestors{
 				      			//innovation is not already consumed in a niche
 				      			.filter(c->!POMs.keySet().parallelStream()
 					      			.map(p->p.innovationGenes)
-								.collect(Collectors.toList())
-								.contains(c.getInnovation()))
+					      			.collect(Collectors.toList())
+					      			.contains(c.getInnovation()))//autobox rollout
 				      			.collect(Collectors.toList());
 		for(ConnectionGene gene : novelInnovations){
 			if(!novelInnovationMap.containsKey(gene.getInnovation()))
 				novelInnovationMap.put(gene.getInnovation(), gene);
 		}
 	}
-	// this can be made adaptable with fitness localization 
-	// wrt discovered fitness landscape. innovationDensity holds
-	// appropriatness information for selection, backtracking 
-	// allows for adaptability
-	public PointOfMutation swapIn(Random r){
+	public PointOfMutation swap(Random r){
 		//selection biased towards innovationDensity
-		List<Integer> swappedOut = POMs.entrySet().parallelStream().filter(e->e.getKey().members.isEmpty())
+		List<Integer> swappedOut = POMs.entrySet().parallelStream()
+//						   .filter(e->e.getKey().members.isEmpty())
 						   .map(e->e.getValue())
 						   .collect(Collectors.toList());
 		Integer selection = swappedOut.get(r.nextInt(swappedOut.size()));
 
-		System.out.println("swapIn: selecting from: " + selection);
+		System.out.println("swap: selecting from: " + selection);
 		//select random POM from innovationDensity	
 		PointOfMutation swap = POMs.entrySet().parallelStream().filter(e->e.getValue().equals(selection))
 					   .collect(Collectors.toList())
 					   .get(r.nextInt((int) POMs.values().parallelStream()
 					   .filter(v->v.equals(selection)).count()))
 					   .getKey();
-		System.out.println("swapIn: Selected POM: " + swap);
+		System.out.println("swap: Selected POM: " + swap);
 		return swap;//add mutated mascot till nextGenGenome.size()==population.size()
-	}
-	private void swapOut(){
-		//TODO: only call when members.isEmpty() to alleviate migrate 
-		//as of now this is just members.clear()
-		for(PointOfMutation swap : POMs.keySet()){
-			swap.members.clear();
-		}
 	}
 }
