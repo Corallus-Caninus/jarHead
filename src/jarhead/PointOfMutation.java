4c6ef0c6d633e4f578a8645c060c780183ceca51
ElectricIsotope <ward.joshua92@yahoo.com>
2019-04-30 20:26:48 -0700

    PointOfMutation: added lifetime and made innovationGenes strictly novel

--- src/jarhead/PointOfMutation.java
+++ src/jarhead/PointOfMutation.java
@@ -1,20 +1,27 @@
 package jarhead;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PointOfMutation{
-	public Map<Integer, ConnectionGene> innovationGenes;
+	public Map<Integer, ConnectionGene> innovationGenes = new HashMap<Integer,ConnectionGene>();
 	public Genome mascot;
 	public Float highScore;
 	public List<Genome> members;
+	public Integer resources, lifetime;
 	
 	//Constructor
-	public PointOfMutation(Float score, Genome newGenome){	
+	public PointOfMutation(Float score, Genome newGenome, List<Integer> novelInnovationGenes) {	
 		mascot = new Genome(newGenome);//snapshot of genome
-		innovationGenes = newGenome.getConnectionGenes();
+//		innovationGenes = newGenome.getConnectionGenes();
+		newGenome.getConnectionGenes().entrySet().parallelStream().filter(e->novelInnovationGenes.contains(e.getKey()))
+				.forEach(e->innovationGenes.put(e.getKey(), newGenome.getConnectionGenes().get(e.getKey())));
 
 		highScore = score;//not highScore as not resetting scores on branch.
 		members = new ArrayList<Genome>();
-		members.add(newGenome); //TODO: getter and setters?
+//		members.add(newGenome); 
+		resources = 1;
+		lifetime = 1;
 	}
+	
 }
 
